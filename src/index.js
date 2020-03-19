8c3f8fec62ac2c97b822338e54ebbc8077c3e43f
Faiq <faiq.kaboel@gmail.com>
2020-03-19 17:00:17 +0700

    final

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { createStore, compose } from 'redux'
+import MainReducer from './reducers/MainReducer'
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+const store = compose(window.devToolsExtension ? window.devToolsExtension() : f => f)(createStore)(MainReducer)
+
+ReactDOM.render(<Provider store={store}>
+<App />
+</Provider>
+, document.getElementById('root'));
+serviceWorker.register();
